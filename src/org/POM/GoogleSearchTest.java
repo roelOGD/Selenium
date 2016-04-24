018a1d8479627b0e3058626ee0c2aea882897b83
Roel <roel.denblanken@ogd.nl>
2016-04-24 12:47:23 +0200

    tweede versie

--- src/org/POM/GoogleSearchTest.java
+++ src/org/POM/GoogleSearchTest.java
@@ -1,8 +1,10 @@
 package org.POM;
 
+import org.DDT.ExcelLib;
 import org.openqa.selenium.WebDriver;
 
 public class GoogleSearchTest extends StaticClass{
+	
 	
 	@Override
 	public WebDriver getDriver() {
@@ -10,14 +12,14 @@ public class GoogleSearchTest extends StaticClass{
 	}
 	
 	public static void main (String[] args) throws InterruptedException{
-		
+		ExcelLib excel = new ExcelLib("C:\\Users\\Roel\\Desktop\\SeleniumExcel.xls");
 		// Ga naar google.com
 		driver.navigate().to("http://google.com");
 
 		// Object for the page
 		// GoogleHomePageObjects page = new GoogleHomePageObjects(driver);
 		GoogleHomePageObjects page = new GoogleHomePageObjects();
-		page.SearchGoogle("Selenium");
+		page.SearchGoogle(excel.GetCellValue(0, 2));
 		
 		Thread.sleep(5000);
 		// Click op de link Selenium web Automation
